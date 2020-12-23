2f5be8a1c1786525e2cdd12b88e85612c57323c3
Mrityunjay Kumar <53057286+mrityunjay-1@users.noreply.github.com>
2020-12-23 13:21:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const server = express();
 
 console.log(process.env.MY_DOMAIN);
 
+server.get('/', (req, res) => {
+    res.send("Hey! This is Fantastic! I deployed a project on AWS.");
+})
+
 server.listen(process.env.PORT, () => {
     console.log("Server is up and running on port:", process.env.PORT);
 })
